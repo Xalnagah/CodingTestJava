816e379b00b495152af8588493b45496fcd268d5
홍성민 <xalnagah@gmail.com>
2022-04-29 18:30:29 +0900

    단체사진 찍기 - Backup

--- src/me/programmers/first/Quest11.java
+++ src/me/programmers/first/Quest11.java
@@ -1,6 +1,7 @@
 package me.programmers.first;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,39 +9,49 @@ public class Quest11 {
 
     List<List<String>> positionList;
 
-    public int solution(int n, String[] data) {
-        int answer = 0;
+    int answer;
+    String[] members = {"A", "C", "F", "J", "M", "N", "R", "T"};
 
-        String[] members = {"A", "C", "F", "J", "M", "N", "R", "T"};
+    String[] chkData;
+
+    public int solution(int n, String[] data) {
+        answer = 0;
+        chkData = data;
+
         int memCnt = members.length;
         positionList = new ArrayList<>();
 
-        makePosition(members, 0);
+//        makePosition(members, 0);
+//        makePosition2(members);
 
-        answer = positionList.stream()
-                .filter(postion -> chkPosition(postion, data))
-                .collect(Collectors.toList())
-                .size();
+//        answer = positionList.stream()
+//                .filter(postion -> chkPosition(postion, data))
+//                .collect(Collectors.toList())
+//                .size();
 
         return answer;
     }
 
-    public void makePosition(String[] members, int startIdx) {
+    public void makePosition(String[] members, int depth) {
+
         int len = members.length;
-        if (startIdx == len -1) {
-            List<String> mLs = new ArrayList<>();
-            for (String m : members) {
-                mLs.add(m);
+        if (depth == len) {
+            if (chkPosition(members, chkData)) {
+                answer++;
             }
-            positionList.add(mLs);
             return;
         }
 
-        for (int i=startIdx; i<len; i++) {
-            swap(members, startIdx, i);
-            makePosition(members, startIdx + 1);
-            swap(members, startIdx, i);
+        for (int i=depth; i<len; i++) {
+            swap(members, depth, i);
+            makePosition(members, depth + 1);
+            swap(members, depth, i);
         }
+    }
+
+
+    public void makePosition2(String[] members, int d, int len) {
+
     }
 
     public void swap(String[] members, int a, int b) {
@@ -49,35 +60,41 @@ public class Quest11 {
         members[b] = tmp;
     }
 
-    public boolean chkPosition(List<String> position, String[] filters) {
+    public boolean chkPosition(String[] position, String[] filters) {
 
         boolean result = false;
 
+//        System.out.println("-------------------------- ");
         for (String filter : filters) {
             String member = filter.substring(0, 1);
             String target = filter.substring(2, 3);
             String comp = filter.substring(3, 4);
-
-            int mIdx = position.indexOf(member);
-            int tIdx = position.indexOf(target);
             int num = Integer.parseInt(filter.substring(4, 5));
+            int mIdx = Arrays.asList(position).indexOf(member);
+            int tIdx = Arrays.asList(position).indexOf(target);
 
-            int between = Math.abs(mIdx - tIdx);
+            int between = Math.abs(mIdx - tIdx) -1;
             switch (comp) {
                 case ">":
                     result = result || (between > num);
+//                    System.out.println(" > " + num + " : " + Arrays.toString(position) + " - " + member + " : " + target + ", between : " + between + ", result : " + result + ", mIdx : " + mIdx + ", tIdx : " + tIdx);
                     break;
                 case "<":
                     result = result || (between < num);
+//                    System.out.println(" < " + num + " : " + Arrays.toString(position) + " - " + member + " : " + target + ", between : " + between + ", result : " + result);
                     break;
-                default: // "="
-                    result = result || (between == 1);
+                case "=": // "="
+                    result = result || (between == num);
+//                    System.out.println(" = " + num + " : " + Arrays.toString(position) + " - " + member + " : " + target + ", between : " + between + ", result : " + result + ", mIdx : " + mIdx + ", tIdx : " + tIdx);
+                    break;
+                default:
                     break;
             }
         }
-
-        return result;
+        return !result;
     }
+
+
 
     public static void main(String[] args) {
 
