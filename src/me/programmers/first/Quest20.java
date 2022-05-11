22ccff74a9f6a88c7d0f155730c41cf19dee21bf
홍성민 <xalnagah@gmail.com>
2022-05-11 18:32:19 +0900

    더 맵게 - Backup

---
+++ src/me/programmers/first/Quest20.java
@@ -0,0 +1,17 @@
+package me.programmers.first;
+
+public class Quest20 {
+
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Quest20 quest20 = new Quest20();
+        // scoville : [1, 2, 3, 9, 10, 12], k : 7, result : 2
+        int[] scoville = {1, 2, 3, 9, 10, 12};
+        System.out.println(">> expected : 2 > result : " + quest20.solution(scoville, 7));
+
+    }
+}
