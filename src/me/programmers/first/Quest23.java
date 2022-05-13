e563b36a49da9b9c456868d90031c6b7835c73d6
홍성민 <xalnagah@gmail.com>
2022-05-13 11:39:16 +0900

    체육복

--- src/me/programmers/first/Quest23.java
+++ src/me/programmers/first/Quest23.java
@@ -1,8 +1,90 @@
 package me.programmers.first;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Quest23 {
     public int solution(int n, int[] lost, int[] reserve) {
-        int answer = 0;
+        int answer = n - lost.length;
+
+        List<Integer> lostList = Arrays.stream(lost)
+                                        .boxed()
+                                        .sorted()
+                                        .collect(Collectors.toList());
+        List<Integer> reserveList = Arrays.stream(reserve)
+                                            .boxed()
+                                            .sorted()
+                                            .collect(Collectors.toList());
+
+        for (int l : lost) {
+            if (reserveList.contains(l)) {
+                lostList.remove((Integer) l);
+                reserveList.remove((Integer) l);
+                answer++;
+            }
+        }
+
+        lost = lostList.stream().mapToInt(l -> l).toArray();
+        reserve = reserveList.stream().mapToInt(r -> r).toArray();
+
+        boolean[] chkReserve = new boolean[reserve.length];
+        for (int i=0; i<lost.length; i++) {
+
+            for (int j=0; j<reserve.length; j++) {
+                if (!chkReserve[j]) {
+                    if ((lost[i]-1) == reserve[j] || (lost[i]+1) == reserve[j]) {
+                        answer++;
+                        chkReserve[j] = true;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return answer;
+    }
+    public int solution2(int n, int[] lost, int[] reserve) {
+        int answer = n - lost.length;
+
+        List<Integer> lostList = new ArrayList<>();
+        for (int l : lost) {
+            lostList.add(l);
+        }
+        Collections.sort(lostList);
+
+        List<Integer> reserveList = new ArrayList<>();
+        for (int r : reserve) {
+            reserveList.add(r);
+        }
+        Collections.sort(reserveList);
+        for (int l : lost) {
+            if (reserveList.contains(l)) {
+                lostList.remove((Integer) l);
+                reserveList.remove((Integer) l);
+                answer++;
+            }
+        }
+
+        lost = lostList.stream().mapToInt(l -> l).toArray();
+        reserve = reserveList.stream().mapToInt(r -> r).toArray();
+
+        boolean[] chkReserve = new boolean[reserve.length];
+        for (int i=0; i<lost.length; i++) {
+
+            for (int j=0; j<reserve.length; j++) {
+                if (!chkReserve[j]) {
+                    if ((lost[i]-1) == reserve[j] || (lost[i]+1) == reserve[j]) {
+                        answer++;
+                        chkReserve[j] = true;
+                        break;
+                    }
+                }
+            }
+        }
+
         return answer;
     }
 
@@ -24,5 +106,15 @@ public class Quest23 {
         int[] lost2 = {3};
         int[] reserve2 = {1};
         System.out.println(">> expected : 2 > result : " + quest23.solution(3, lost2, reserve2));
+
+        // n : 3, lost : [3], reserve : [1], return : 2
+        int[] lost3 = {1, 2, 4};
+        int[] reserve3 = {2, 4, 5};
+        System.out.println(">> expected : 4 > result : " + quest23.solution(5, lost3, reserve3));
+
+        // n : 3, lost : [3], reserve : [1], return : 2
+        int[] lost4 = {1, 2, 4};
+        int[] reserve4 = {2, 3, 4, 5};
+        System.out.println(">> expected : 4 > result : " + quest23.solution(5, lost4, reserve4));
     }
 }
