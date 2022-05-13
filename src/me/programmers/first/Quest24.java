6b4674d27fedee855ffebfef8b2fa0a829218ecd
홍성민 <xalnagah@gmail.com>
2022-05-13 18:26:17 +0900

    N으로 표현

---
+++ src/me/programmers/first/Quest24.java
@@ -0,0 +1,19 @@
+package me.programmers.first;
+
+public class Quest24 {
+    public int solution(int N, int number) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        Quest24 quest24 = new Quest24();
+
+        // N : 5, number : 12, return : 4
+        System.out.println(">> expected : 4 > result : " + quest24.solution(5, 12));
+        // N : 2, number : 11, return : 3
+        System.out.println(">> expected : 3 > result : " + quest24.solution(2, 11));
+
+    }
+}
